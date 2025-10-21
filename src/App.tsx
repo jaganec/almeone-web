d306c71304f00a8607772923868e4977dfa0e05e
Tandra2821 <bhargavi.tandra028@gmail.com>
2025-10-21 17:59:46 -0500

    Added invisible reCAPTCHA integration and .env setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,23 @@ import Services from './components/Services';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <Services />
-      <About />
-      <Contact />
-      <Footer />
-    </div>
+    
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.REACT_APP_RECAPTCHA_SITE_KEY as string}
+      scriptProps={{ async: true, defer: true }}
+    >
+      <div className="App">
+        <Header />
+        <Hero />
+        <Services />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
+    </GoogleReCaptchaProvider>
   );
 };
 
